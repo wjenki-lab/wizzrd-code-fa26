9351465036db8ca3f5b441cb98e5c7a97abfd400
wjenki-lab <wjenki@artic.edu>
2026-08-26 14:45:49 -0500

    some bullshit

--- home.js
+++ home.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+console.log("hello world")[]
+
+function setup(){
+    createCanvas(windowWidth, 
+        windowHeight)
+}
+
+function draw() {
+    //background(400)
+    rectMode(CENTER)
+    rect(mouseX, mouseY, 50, 50)
+function windowResized() {}    
+}
